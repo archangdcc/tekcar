4604117f9bbdcddf6dfe67ef8df4a39ef62fd541
Jeremy Siek <jsiek@indiana.edu>
2016-02-22 23:05:29 -0500

    tested debug-level

--- run-tests.rkt
+++ run-tests.rkt
@@ -4,5 +4,7 @@
 (require "interp.rkt")
 (require "compiler.rkt")
 
+;; (debug-level 4)
+
 (interp-tests "integers and arithmetic" #f r0-passes interp-scheme "r0" (range 1 5))
 (display "tests passed!") (newline)
