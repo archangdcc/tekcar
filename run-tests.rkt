84d4e7c95802a53d8720c27a32d222dbc249bc0d
archang <archangdcc@gmail.com>
2017-08-26 16:22:37 -0400

    E3: Add more tests for uniquification

--- run-tests.rkt
+++ run-tests.rkt
@@ -10,5 +10,5 @@
 (interp-tests "integers and arithmetic" #f r0-passes interp-scheme "r0" (range 1 5))
 (display "tests passed!") (newline)
 
-(interp-tests "integers and arithmetic" #f r1-passes interp-scheme "r1" (range 1 22))
+(interp-tests "integers and arithmetic" #f r1-passes interp-scheme "r1" (range 1 27))
 (display "tests passed!") (newline)
