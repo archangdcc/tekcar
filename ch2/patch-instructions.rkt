084572bf10a4a463d16483e5cbb07138807446b6
archang <archangdcc@gmail.com>
2017-08-30 14:01:27 -0400

    patch-instr: no hardcoding of temp reg

--- ch2/patch-instructions.rkt
+++ ch2/patch-instructions.rkt
@@ -2,12 +2,13 @@
 
 (provide patch-instructions)
 
+(define temp-reg 'rax)
 (define (patch-instructions e)
   (match e
     ['() '()]
     [`((,op (deref ,reg₁ ,offset₁) (deref ,reg₂ ,offset₂)) . ,instrs)
-     `((movq (deref ,reg₁ ,offset₁) (reg rax))
-       (,op (reg rax) (deref ,reg₂ ,offset₂)) .
+     `((movq (deref ,reg₁ ,offset₁) (reg ,temp-reg))
+       (,op (reg ,temp-reg) (deref ,reg₂ ,offset₂)) .
        ,(patch-instructions instrs))]
     [`(,instr . ,instrs)
      `(,instr . ,(patch-instructions instrs))]
