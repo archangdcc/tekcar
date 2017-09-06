1df46d388aa010d032ba08338b236cc87c92b72c
archang <archangdcc@gmail.com>
2017-09-05 23:27:37 -0400

    select-instructions: simplify cases

--- ch2/select-instructions.rkt
+++ ch2/select-instructions.rkt
@@ -4,61 +4,37 @@
 
 (define (select-instructions e)
   (match e
+    [`,n #:when (integer? n)
+     `(int ,n)]
+    [`,x #:when (symbol? x)
+     `(var ,x)]
     ['() '()]
     [`((assign ,lhs (read)) . ,instr)
      `((callq read_int)
        (movq (reg rax) (var ,lhs)) .
        ,(select-instructions instr))]
-    [`((assign ,x ,n) . ,instr) #:when (integer? n)
-     `((movq (int ,n) (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x ,y) . ,instr) #:when (symbol? y)
-     `((movq (var ,y) (var ,x)) .
-       ,(select-instructions instr))]
     [`((assign ,x (- ,x)) . ,instr)
      `((negq (var ,x)) .
        ,(select-instructions instr))]
-    [`((assign ,x (- ,n)) . ,instr) #:when (integer? n)
-     `((movq (int ,n) (var ,x))
-       (negq (var ,x)) .
-       ,(select-instructions instr))]
     [`((assign ,x (- ,y)) . ,instr)
-     `((movq (var ,y) (var ,x))
+     `((movq ,(select-instructions y) (var ,x))
        (negq (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x (+ ,n ,x)) . ,instr) #:when (integer? n)
-     `((addq (int ,n) (var ,x)) .
        ,(select-instructions instr))]
     [`((assign ,x (+ ,y ,x)) . ,instr)
-     `((addq (var ,y) (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x (+ ,x ,n)) . ,instr) #:when (integer? n)
-     `((addq (int ,n) (var ,x)) .
+     `((addq ,(select-instructions y) (var ,x)) .
        ,(select-instructions instr))]
     [`((assign ,x (+ ,x ,y)) . ,instr)
-     `((addq (var ,y) (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x (+ ,m ,n)) . ,instr) #:when (and (integer? m) (integer? n))
-     `((movq (int ,m) (var ,x))
-       (addq (int ,n) (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x (+ ,m ,y)) . ,instr) #:when (integer? m)
-     `((movq (int ,m) (var ,x))
-       (addq (var ,y) (var ,x)) .
-       ,(select-instructions instr))]
-    [`((assign ,x (+ ,y ,n)) . ,instr) #:when (integer? n)
-     `((movq (int ,n) (var ,x))
-       (addq (var ,y) (var ,x)) .
+     `((addq ,(select-instructions y) (var ,x)) .
        ,(select-instructions instr))]
     [`((assign ,x (+ ,y ,z)) . ,instr)
-     `((movq (var ,y) (var ,x))
-       (addq (var ,z) (var ,x)) .
+     `((movq ,(select-instructions y) (var ,x))
+       (addq ,(select-instructions z) (var ,x)) .
        ,(select-instructions instr))]
-    [`((return ,n) . ,instr) #:when (integer? n)
-     `((movq (int ,n) (reg rax)) .
+    [`((assign ,x ,y) . ,instr)
+     `((movq ,(select-instructions y) (var ,x)) .
        ,(select-instructions instr))]
-    [`((return ,x) . ,instr)
-     `((movq (var ,x) (reg rax)) .
+    [`((return ,y) . ,instr)
+     `((movq ,(select-instructions y) (reg rax)) .
        ,(select-instructions instr))]
     [`(program ,var . ,instr)
      `(program ,var . ,(select-instructions instr))]))
