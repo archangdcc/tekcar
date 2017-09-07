12b9687b094b3634f2f9647a3037e2357ce1c359
archang <archangdcc@gmail.com>
2017-09-07 19:31:14 -0400

    E10: implement of build-interference

---
+++ ch3/build-interference.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require "../utilities.rkt")
+(require "../global.rkt")
+
+(provide build-interference)
+
+(define (build-graph graph live-after instrs)
+  (for-each
+    (lambda (live instr)
+      (match instr
+        [`(movq (var ,y) (var ,x))
+          (set-for-each live
+            (lambda (v)
+              (cond
+                [(eq? v x) (void)]
+                [(eq? v y) (void)]
+                [else (add-edge graph x v)])))]
+        [`(,op ... (var ,x))
+          (set-for-each live
+            (lambda (v)
+              (cond
+                [(eq? v x) (void)]
+                [else (add-edge graph x v)])))]
+        [`(callq ,label)
+          (set-for-each live
+            (lambda (v)
+              (for-each
+                (lambda (r)
+                  (add-edge graph r v))
+                caller-regs)))]
+        [_ (void)]))    ;; may need extra work for reg args
+    live-after instrs))
+
+(define (build-interference p)
+  (match p
+    [`(program (,vars ... ,live-after) . ,instrs)
+      (let ([graph
+             (make-graph (append vars callee-regs caller-regs))])
+        (build-graph graph live-after instrs)
+        `(program (,@vars ,graph) . ,instrs))]))
--- run-tests.rkt
+++ run-tests.rkt
@@ -12,7 +12,7 @@
 (display "\e[0;34;42mr0-passes: tests passed!\e[0m") (newline)
 
 
-(debug-level 1)
+(debug-level 0)
 
 (interp-tests "integers and variables" #f r1-passes interp-scheme "r1" (range 1 30))
 (display "\e[0;34;42mr1-passes: interp tests passed!\e[0m") (newline)
