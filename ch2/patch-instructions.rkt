74a0117c7edded19d7ecf330878d747499813f54
archang <archangdcc@gmail.com>
2017-09-07 16:49:31 -0400

    patch-instructions: simplify by using foldr

--- ch2/patch-instructions.rkt
+++ ch2/patch-instructions.rkt
@@ -3,14 +3,15 @@
 (require "../global.rkt")
 (provide patch-instructions)
 
+(define (patch-instr instr tail)
+  (match instr
+    [`(,op (deref ,reg₁ ,offset₁) (deref ,reg₂ ,offset₂))
+     `((movq (deref ,reg₁ ,offset₁) (reg ,temp-reg))
+       (,op (reg ,temp-reg) (deref ,reg₂ ,offset₂)) . ,tail)]
+    [_ (cons instr tail)]))
+
 (define (patch-instructions e)
   (match e
-    ['() '()]
-    [`((,op (deref ,reg₁ ,offset₁) (deref ,reg₂ ,offset₂)) . ,instrs)
-     `((movq (deref ,reg₁ ,offset₁) (reg ,temp-reg))
-       (,op (reg ,temp-reg) (deref ,reg₂ ,offset₂)) .
-       ,(patch-instructions instrs))]
-    [`(,instr . ,instrs)
-     `(,instr . ,(patch-instructions instrs))]
-    [`(program ,n . ,instrs)
-     `(program ,n . ,(patch-instructions instrs))]))
+    [`(program ,infos . ,instrs)
+     `(program ,infos .
+       ,(foldr patch-instr '() instrs))]))
