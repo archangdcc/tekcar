c75067cb8c863992e6924a89f929fb66c00bd28a
archang <archangdcc@gmail.com>
2017-10-06 23:40:54 -0400

    R3 flatten: fix collect, if cmp

--- ch5/flatten.rkt
+++ ch5/flatten.rkt
@@ -21,50 +21,53 @@
 
 (define (flat-if condition thns elss)
   (match condition
-    [(? boolean?)
-     (if condition (flat thns) (flat elss))]
-    [`(let ([,x ,e]) ,body)
-      (flat `(let ([,x ,e]) (if ,body ,thns ,elss)))]
-    [`(if ,condition ,ithns ,ielss)
-      (let-values
-        ([(ret_c assign_c var_c) (flat-if condition ithns ielss)]
-         [(ret_t assign_t var_t) (flat thns)]
-         [(ret_e assign_e var_e) (flat elss)])
-       (let ([y (gen-sym 'if)])
-        (values y
-          (append assign_c
-           `((if (eq? ,ret_c #t)
-               ,(append assign_t `((assign ,y ,ret_t)))
-               ,(append assign_e `((assign ,y ,ret_e))))))
-          (append var_c `((,y . ,(caddr thns))) var_t var_e))))]
-    [`(and ,x ,y) (flat-if `(if ,x ,y (has-type #f Boolean)) thns elss)]
-    [`(not (has-type ,condition ,t))
-      (flat-if condition elss thns)]
-    [`(,cmp ,a ,b)
-      (let-values
-        ([(ret_a assign_a var_a) ((malloc flat) a)]
-         [(ret_b assign_b var_b) ((malloc flat) b)]
-         [(ret_t assign_t var_t) (flat thns)]
-         [(ret_e assign_e var_e) (flat elss)])
-        (let ([y (gen-sym 'if)])
-          (values y
-            (append assign_a assign_b
-             `((if (,cmp ,ret_a ,ret_b)
-                 ,(append assign_t `((assign ,y ,ret_t)))
-                 ,(append assign_e `((assign ,y ,ret_e))))))
-            (append `((,y . ,(caddr thns))) var_a var_b var_t var_e))))]
-    [_
-     (let-values
-       ([(ret_c assign_c var_c) ((malloc flat) condition)]
-        [(ret_t assign_t var_t) (flat thns)]
-        [(ret_e assign_e var_e) (flat elss)])
-       (let ([y (gen-sym 'if)])
-         (values y
-           (append assign_c
-             `((if (eq? ,ret_c #t)
-                 ,(append assign_t `((assign ,y ,ret_t)))
-                 ,(append assign_e `((assign ,y ,ret_e))))))
-           (append var_c `((,y . ,(caddr thns))) var_t var_e))))]))
+    [`(has-type ,conditon Boolean)
+      (match condition
+        [(? boolean?)
+         (if condition (flat thns) (flat elss))]
+        [`(let ([,x ,e]) ,body)
+          (flat `(let ([,x ,e]) (if ,body ,thns ,elss)))]
+        [`(if ,condition ,ithns ,ielss)
+          (let-values
+            ([(ret_c assign_c var_c) (flat-if condition ithns ielss)]
+             [(ret_t assign_t var_t) (flat thns)]
+             [(ret_e assign_e var_e) (flat elss)])
+           (let ([y (gen-sym 'if)])
+            (values y
+              (append assign_c
+               `((if (eq? ,ret_c #t)
+                   ,(append assign_t `((assign ,y ,ret_t)))
+                   ,(append assign_e `((assign ,y ,ret_e))))))
+              (append var_c `((,y . ,(caddr thns))) var_t var_e))))]
+        [`(and ,x ,y) (flat-if `(if ,x ,y (has-type #f Boolean)) thns elss)]
+        [`(not ,condition )
+          (flat-if condition elss thns)]
+        [`(,cmp ,a ,b)
+          #:when(set-member? (set 'eq? '< '> '<= '>=) cmp)
+          (let-values
+            ([(ret_a assign_a var_a) ((malloc flat) a)]
+             [(ret_b assign_b var_b) ((malloc flat) b)]
+             [(ret_t assign_t var_t) (flat thns)]
+             [(ret_e assign_e var_e) (flat elss)])
+            (let ([y (gen-sym 'if)])
+              (values y
+                (append assign_a assign_b
+                 `((if (,cmp ,ret_a ,ret_b)
+                     ,(append assign_t `((assign ,y ,ret_t)))
+                     ,(append assign_e `((assign ,y ,ret_e))))))
+                (append `((,y . ,(caddr thns))) var_a var_b var_t var_e))))]
+        [_
+         (let-values
+           ([(ret_c assign_c var_c) ((malloc flat) condition)]
+            [(ret_t assign_t var_t) (flat thns)]
+            [(ret_e assign_e var_e) (flat elss)])
+           (let ([y (gen-sym 'if)])
+             (values y
+               (append assign_c
+                 `((if (eq? ,ret_c #t)
+                     ,(append assign_t `((assign ,y ,ret_t)))
+                     ,(append assign_e `((assign ,y ,ret_e))))))
+               (append var_c `((,y . ,(caddr thns))) var_t var_e))))])]))
 
 (define (flat e)
   (match e
@@ -74,7 +77,7 @@
     [`(allocate ,n ,type)
        (values e '() '())]
     [`(collect ,n)
-       (values e '() '())]
+       (values '(void) `((collect ,n)) '())]
     [x #:when (or (symbol? x) (integer? x) (boolean? x))
        (values x '() '())]
     [`(let ([,x (has-type ,e ,t)]) ,body)
@@ -85,8 +88,8 @@
           (append assign₁ `((assign ,x ,ret₁)) assign₂)  ;; always assign in let clause
           (append var₁ `((,x . ,t)) var₂)))]
     [`(and ,e₁ ,e₂)
-      (flat `(if ,e₁ ,e₂ #f))]
-    [`(if (has-type ,condition ,t) ,thns ,elss)
+      (flat `(if ,e₁ ,e₂ (has-type #f Boolean)))]
+    [`(if ,condition ,thns ,elss)
       (flat-if condition thns elss)]
     [`(program ,type ,e)
       (let-values ([(ret assign var) ((malloc flat) e)])    ;; only assign to non-arg in return
