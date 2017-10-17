8b57dca88fd26893f461465cfe3af33d747edb09
archang <archangdcc@gmail.com>
2017-10-17 07:29:07 -0400

    add lower-conditionals to R3

---
+++ ch5/lower-conditionals.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require "../global.rkt")
+
+(provide lower-conditionals-R3)
+
+
+(define (lower-cond instr tail)
+  (match instr
+    [`(if (,cmp ,x ,y) ,thns ,elss)
+     (let ([thnlabel (gen-sym 'then)]
+           [endlabel (gen-sym 'if_end)])
+       `((cmpq ,y ,x)
+         (jmp-if ,(cc cmp) ,thnlabel)
+         ,@(foldr lower-cond '() elss)
+         (jmp ,endlabel)
+         (label ,thnlabel)
+         ,@(foldr lower-cond '() thns)
+         (label ,endlabel) .
+         ,tail))]
+    [_ (cons instr tail)]))
+
+(define (lower-conditionals-R3 p)
+  (match p
+    [`(program ,info ,type . ,instrs)
+     `(program ,info ,type .
+       ,(foldr lower-cond '() instrs))]))
